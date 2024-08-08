56fffe406ca60f708b9c29453592370ce210aa81
mac <mac@WHMacBook-Pro.local>
2024-08-08 19:55:45 +0800

    Initial commit

--- ui/src/utils/addressUtils.js
+++
@@ -1,6 +0,0 @@
-export function getShortAddress(address) {
-  const firstPart = address.slice(0, 5);
-  const lastPart = address.slice(address.length-4, address.length);
-  
-  return firstPart + '...' + lastPart;
-}
